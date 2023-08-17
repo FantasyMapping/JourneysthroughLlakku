9cfdb877ac4fdf4488fee9ff9ab79e2d0ca40132
Timothy Pelham <timtitan@gmail.com>
2023-08-17 14:16:57 +0100

    Added Winter Campaign
    Added Winter Campaign map, as a prelude to the timed map.

---
+++ Generation Scripts/kingdomofthewhite.py
@@ -0,0 +1,42 @@
+
+import folium
+from folium.plugins import MeasureControl, MousePosition, MiniMap
+from pathlib import Path
+import matplotlib.pyplot as plt
+from pathlib import Path
+from scipy.interpolate import griddata
+import geojsoncontour
+import numpy as np
+import scipy as sp
+import scipy.ndimage
+import branca
+
+
+
+white_url = Path('../Kingdom of the White Map/Kingdom Of the WhiteScaled.png')
+white_url2 = Path('../Kingdom of the White Map/Kingdom Of the White.png')
+# Create a map centered at a specific location
+tile_url = Path('../Kingdom of the White Map/tiles/{z}/{x}/{-y}.png')
+tile_url2 = Path('./World Map/tileshires/{z}/{x}/{-y}.png')
+# Create a map centered at a specific location
+m = folium.Map(location=[70, -90], zoom_start=6, min_zoom=6, max_zoom=10,control_scale = True,tiles=None)
+folium.TileLayer(tiles=tile_url.as_posix(),attr='Russell',crs='EPSG3857', name='Kingdom of the White').add_to(m)
+folium.TileLayer(tiles=tile_url2.as_posix(),attr='Russell',crs='EPSG3857', name='World Map').add_to(m)
+
+
+folium.LayerControl().add_to(m)
+formatter = "function(num) {return L.Util.formatNum(num, 5) + ' º ';};"
+#MeasureControl().add_to(m)
+folium.plugins.MousePosition(
+    position="topright",
+    separator=" | ",
+    empty_string="NaN",
+    lng_first=True,
+    num_digits=20,
+    prefix="Coordinates:",
+    lat_formatter=formatter,
+    lng_formatter=formatter,
+).add_to(m)
+
+# Display the map
+m.save("../KingdomoftheWhite.html")
